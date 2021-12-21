2ded538668a1ef74b190d5050d794c08e4d7a085
Entreprenerd <valeranijr@gmail.com>
2021-12-21 02:48:31 +0100

    fix: fixed pragma

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import {BaseStrategy} from "@badger-finance/BaseStrategy.sol";
